83f5a450464d216ae11f57e40b57f6efca674f83
Jerry <j3rry15@gmail.com>
2023-08-29 10:06:25 +0100

    First Pass

--- functional-samples/my.test.extension/popup.js
+++ functional-samples/my.test.extension/popup.js
@@ -1,19 +1,8 @@
-let changeColor = document.getElementById("changeColor");
 
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
-
-chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-    await chrome.sidePanel.setOptions({
-      tabId,
-      path: 'sidepanel.html',
-      enabled: true
-    });
-  });
-
+let changeColor = document.getElementById("getInfo");
 changeColor.addEventListener("click", async() => {
   chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
+
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     await chrome.scripting.executeScript({
       target: { tabId: tab.id },
@@ -21,11 +10,14 @@ changeColor.addEventListener("click", async() => {
     });
      chrome.storage.session.get("pageTitle",({pageTitle}) => {
       $("body").append("<p>"+pageTitle+"</p>");
-    });
+    }); 
+    
  });
  
 function storeVariables() {
     chrome.storage.session.set({ pageTitle: document.title }).then(() => {
       console.log("Value was set" + document.title);
     }); 
+
 }
+ 
