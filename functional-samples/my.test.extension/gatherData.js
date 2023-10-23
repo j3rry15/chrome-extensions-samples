a8e62eddd521571053f7f6c9e87b8504fd0de455
Jerry <j3rry15@gmail.com>
2023-10-23 16:43:10 +0100

    Remove unnecessary files

--- functional-samples/my.test.extension/gatherData.js
+++ functional-samples/my.test.extension/gatherData.js
@@ -12,7 +12,7 @@ getInfo.addEventListener("click", async() => {
       if(!document.getElementById("pageTitleTest")){
       $("body").append("<div id = 'pageTitleTest'><p> Used for Logic to store variables between page and extension. <br> Example: PageTitle="+pageTitle+"</p><div>");
       }else{
-        $("#pageTitleTest").replaceWith("<p> Used for Logic to store variables between page and extension. <br> Example: PageTitle="+pageTitle+"</p>");
+        $("#pageTitleTest").replaceWith("<div id = 'pageTitleTest'><p> Used for Logic to store variables between page and extension. <br> Example: PageTitle="+pageTitle+"</p><div>");
       }
     }); 
     
@@ -25,19 +25,22 @@ dataButton.addEventListener("click", async() => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     await chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: openPage
+      function: openModelsPage
     });
     
  });
 
  // Store the variable that will be used to populate the elements in the sidebar
+ // ToDo:
 function storeVariables() {
     chrome.storage.session.set({ pageTitle: document.title }).then(() => {
       console.log("Value was set " + document.title);
     }); 
 
 }
-function openPage() {
+
+function openModelsPage() {
+  baseURL = window.location.origin
   if (document.URL.indexOf("/profile")!=-1){
     model = "candidate_profile"
     term = document.URL.split('/')[4].split('?')[0]
@@ -48,5 +51,15 @@ function openPage() {
   alert("We can't open the models page as the Page URL doesn't looks like a Profile or Position. Please reach out to jmoloney@eightfold.ai with STR to update.")
   return
  }
- window.open("https://app.eightfold.ai/models?model=" + model + "&terms=" + term)
+ window.open(baseURL + "/models?model=" + model + "&terms=" + term)
+}
+
+// ToDo: get ENG help to make this robust
+function getProfileAtsIdInTa(){
+  profileData=document.getElementById("profile-basic-info").outerHTML.toString()
+  profileId = profileData.indexOf("data-profile-id")
+  profileId = profileData.substring(profileId,profileId+100).split('"')[1]
+
+  let profileATSID = document.getElementsByClassName("profile-ats-id")[0].innerText
+  return profileATSID
 }
